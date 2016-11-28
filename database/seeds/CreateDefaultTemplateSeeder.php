e5bbeda02fb5432a502880cb36cefc9047ab3b06
Aldren Terante <aldren.terante@gmail.com>
2016-11-28 03:49:18 +0000

    update dummy data to be use

--- database/seeds/CreateDefaultTemplateSeeder.php
+++ database/seeds/CreateDefaultTemplateSeeder.php
@@ -13,8 +13,21 @@ class CreateDefaultTemplateSeeder extends Seeder
      */
     public function run()
     {
+        // $this->createDefaultOnlyTemplates();
+        $this->addCustomTemplates();
+    }
+
+    private function createDefaultOnlyTemplates()
+    {
+        $this->templateFullWidth();
+        $this->templateTwoColumn(true);
+    }
+
+    private function addCustomTemplates()
+    {
         $this->templateFullWidth();
         $this->templateTwoColumn();
+        $this->customTemplateTwoColumn();
     }
 
     private function templateFullWidth()
@@ -33,7 +46,10 @@ class CreateDefaultTemplateSeeder extends Seeder
                     ($option->id == 1 && $setting->id == 2) ||
                     ($option->id == 1 && $setting->id == 3) ||
                     ($option->id == 1 && $setting->id == 4) ||
-                    ($option->id == 2 && $setting->id == 4)
+                    ($option->id == 2 && $setting->id == 11) ||
+                    ($option->id == 2 && $setting->id == 12) ||
+                    ($option->id == 2 && $setting->id == 13) ||
+                    ($option->id == 2 && $setting->id == 14)
                 ) {
                     $selected = true;
                 }
@@ -48,11 +64,44 @@ class CreateDefaultTemplateSeeder extends Seeder
 
     }
 
-    private function templateTwoColumn()
+    private function templateTwoColumn($selected = false)
     {
         $template = factory(App\Template::class, 1)->create([
             'type' => 'two-column',
             'category' => 'default',
+            'selected' => $selected
+        ]);
+
+        foreach (Option::all() as $option) {
+            foreach ($option->settings as $setting) {
+                $selected = false;
+                if (
+                    ($option->id == 1 && $setting->id == 1) ||
+                    ($option->id == 1 && $setting->id == 2) ||
+                    ($option->id == 1 && $setting->id == 3) ||
+                    ($option->id == 1 && $setting->id == 4) ||
+                    ($option->id == 2 && $setting->id == 11) ||
+                    ($option->id == 2 && $setting->id == 12) ||
+                    ($option->id == 2 && $setting->id == 13) ||
+                    ($option->id == 2 && $setting->id == 14)
+                ) {
+                    $selected = true;
+                }
+                factory(App\TemplateDetail::class, 1)->create([
+                    'template_id' => $template->id,
+                    'option_id' => $option->id,
+                    'setting_id' => $setting->id,
+                    'selected' => $selected
+                ]);
+            }
+        }
+    }
+
+    private function customTemplateTwoColumn()
+    {
+        $template = factory(App\Template::class, 1)->create([
+            'type' => 'two-column',
+            'category' => 'custom',
             'selected' => true
         ]);
 
@@ -64,7 +113,16 @@ class CreateDefaultTemplateSeeder extends Seeder
                     ($option->id == 1 && $setting->id == 2) ||
                     ($option->id == 1 && $setting->id == 3) ||
                     ($option->id == 1 && $setting->id == 4) ||
-                    ($option->id == 2 && $setting->id == 4)
+                    ($option->id == 2 && $setting->id == 11) ||
+                    ($option->id == 2 && $setting->id == 12) ||
+                    ($option->id == 2 && $setting->id == 13) ||
+                    ($option->id == 2 && $setting->id == 14) ||
+                    ($option->id == 3 && $setting->id == 15) ||
+                    ($option->id == 3 && $setting->id == 16) ||
+                    ($option->id == 3 && $setting->id == 17) ||
+                    ($option->id == 3 && $setting->id == 18) ||
+                    ($option->id == 3 && $setting->id == 19) ||
+                    ($option->id == 3 && $setting->id == 20)
                 ) {
                     $selected = true;
                 }
